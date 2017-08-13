7cee13f80e3ba5bc7a6ad33494a4b35e44ebbe8a
Sun1ive <sunliveua@gmail.com>
2017-08-13 23:01:01 +0300

    1.81 + fix js

--- _scripts/common.js
+++ _scripts/common.js
@@ -35,8 +35,37 @@ $(document).ready(function() {
 		}
 	});
 
-	$('.buttonLoad').click(function(e) {
-		alert('Loading Content...');
+	$('body').on('click','.buttonLoad', function() {
+		//кнопку на которую нажали
+		let button = $(this);
+		// контейнер для товара
+		let container = $('.shopWrapper-item');
+		// ссылка для ajax запроса
+		if(!button.hasClass('working')) {
+			button.addClass('working');
+			let data = {};
+			data.action = 'more';
+			// ajax request
+			$.ajax({
+				url: "postLink",
+				type: "POST",
+				dataType: "json",
+				data: data,
+				success: function(data) {
+					console.log('its fine');
+				},
+				error: function(data) {
+					button.removeClass('working');
+				}
+			}).done(function(data) {
+				container.append(data.html);
+				if(data.last) {
+					button.removeClass('working');
+				} else {
+					button.remove();
+				}
+			});
+		}
 	});
 
 
